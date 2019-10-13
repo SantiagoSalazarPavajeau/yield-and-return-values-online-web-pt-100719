e70838cd162c66d2baf86f757a178aa06fc7487d
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 21:12:16 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,7 @@
 require 'pry'
-
+# we are making our own each and collect/map methods
+# each returns nil because it was only putsing a String
+# collect/map puts every element/iteration into a new array without putsing so returns the array and its elements
 def hello(array)
   i = 0
   collection = []
